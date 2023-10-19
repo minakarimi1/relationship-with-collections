6ccad6b6b4b5b8d73ddae3cf10c9d1662df747ac
minakarimi1 <minakarimi1@yahoo.com>
2023-10-20 00:18:48 +0330

    added Books, Users documents on mongo db

--- relation.js
+++ relation.js
@@ -2,10 +2,27 @@ import 'dotenv/config';
 import express from 'express';
 const app = express();
 const port = process.env.PORT || 1313;
-import mongoose from 'mongoose';
+import mongoose, { Schema } from 'mongoose';
+
+mongoose.connect('mongodb://127.0.0.1:27017/relation')
+.then(()=>console.log('connect to mongo DB'))
+.catch((err)=>console.log('could not to database') + err)
 
 
 
+ const Books = mongoose.model('Books',new mongoose.Schema({
+  title: {type: String},
+  pages: {type: String},
+  }));
+
+  const Users = mongoose.model('Users',new mongoose.Schema({
+    first_name: {type: String},
+    last_name: {type: String},
+    book:{
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Books",
+    },
+  }));
 
 
 
